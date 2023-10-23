78fb935ebd412089aa53c3f1793bbabd5475bfd6
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-10-23 19:21:39 +0700

    Menambah Code Awal Kasus Liskov

---
+++ lsp/Vegetable.java
@@ -0,0 +1,14 @@
+package lsp;
+
+import java.util.Date;
+
+class Vegetable extends Product {
+    @Override
+    String getName() {
+    return "Broccoli";
+    }
+    @Override
+    Date getExpiredDate() {
+    return new Date();
+    }
+}
