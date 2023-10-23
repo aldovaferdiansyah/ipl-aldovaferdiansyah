c45fadc640556d7af93593db7ec2188aa919017d
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-10-23 19:28:21 +0700

    Perubahan Sesuai Princip Liscov

--- lsp/Product.java
+++ lsp/Product.java
@@ -1,14 +1,11 @@
 package lsp;
 
-import java.util.Date;
-
 abstract class Product {
     abstract String getName();
-    abstract Date getExpiredDate();
-/**
-* Function to get all of information about product
-*/
+    /**
+    * Function to get all of information about product
+    */
     public void getProductInfo() {
-// some magic code
-}
+    // some magic code
+    }
 }
