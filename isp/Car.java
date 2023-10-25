432046e959186b48e069e03b82b16a3d293b1417
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-10-25 10:24:39 +0700

    Create ISP

---
+++ isp/Car.java
@@ -0,0 +1,21 @@
+package isp;
+
+public class Car implements VehicleInterface {
+    // Can be implemented
+    @Override
+    public void drive() {
+
+    }
+    @Override
+    public void stop() {
+
+    }
+    @Override
+    public void refuel() {
+
+    }
+    @Override
+    public void openDoors() {
+        
+    }
+}
