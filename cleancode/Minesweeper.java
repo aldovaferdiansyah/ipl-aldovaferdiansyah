84ac3d938d04747c37eef416abb60c59959ffab3
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-11-08 10:57:34 +0700

    Code Awal Costumer

--- cleancode/Minesweeper.java
+++ cleancode/Minesweeper.java
@@ -3,7 +3,6 @@ package cleancode;
 public class Minesweeper(){
     public final static int STATUS_VALUE = 0;
     public final static int FLAGGED = 4;
-
     public List<int[]> getlaggedCells(){
         List<int[]> flaggedCells = new ArrayList<int[]>();
             for (int[] cell : gameBoard)
