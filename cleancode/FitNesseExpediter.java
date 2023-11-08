45264391773886f985f11332ad0cadb19f71c55e
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-11-08 11:39:54 +0700

    Contoh Good Formatting

--- cleancode/FitNesseExpediter.java
+++ cleancode/FitNesseExpediter.java
@@ -1,15 +1,15 @@
 package cleancode;
 
 public class FitNesseExpediter implements ResponseSender{
-    // BAD FORMATTING
-    private    Socket          socket;
-    private    InputStream     input;
-    private    OutputStream    output;
-    private    Request         request;
-    private    Response        response;
-    private    FitNesseContext context;
-    protected  long            requestParsingTimeLimit;
-    private    long            requestProgress;
-    private    long            requestParsingDeadline;
-    private    boolean         hasError;
+    // GOOD FORMATTING
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+    private Request request;
+    private Response response;
+    private FitNesseContext context;
+    protected long requestParsingTimeLimit;
+    private long requestProgress;
+    private long requestParsingDeadline;
+    private boolean hasError;
 }
