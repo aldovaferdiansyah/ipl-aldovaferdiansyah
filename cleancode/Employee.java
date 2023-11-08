5d3a6a35649e99e398c89d6e49f8dc00703b2fc0
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-11-08 11:08:14 +0700

    Perubahan Code Employee

--- cleancode/Employee.java
+++ cleancode/Employee.java
@@ -1,16 +1,17 @@
 package cleancode;
 
 class Employee {
-    int payAmount(){
-        swicth (getType()){
-            case EmployeeType.ENGINEER:
-                return _month1ySalary;
-            case EmployeeType.SALESMAN:
-                return _month1ySalary + _commission;
-            case Employee.MANAGER:
-                return _month1ySalary + _bonus;
-            default:
-                throw new Exception("Incorrect Employee");
+    class EmployeeType {
+        abstract int payAmount(Employee emp);
+    }
+    class Salesman{
+        int payAmount(Employee emp){
+            return emp.getMonth1Salary() + emp.getCommission();
+        }
+    }
+    class Manager {
+        int payAmount(Employee emp){
+            return emp.getMonth1Salary() + emp.getBonus();
         }
     }
 }
