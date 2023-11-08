53ba1582961f6859cdaa19b0341784136f585c00
aldovaferdiansyah <aldovaferdiansyah12@gmail.com>
2023-11-08 11:32:59 +0700

    Good Comment

--- cleancode/Comment.java
+++ cleancode/Comment.java
@@ -1,17 +1,21 @@
 package cleancode;
 
 public class Comment {
-    /**
-     * Default constructor.
-     */
-    protected AnnualDateRule(){ }
-    /** The Day of the month. */
-    private int dayOfMonth;
-    /**
-     * Returns the day of the month.
-     * @return the day of the month.
-     */
-    public int getDayOfMonth(){
-        return dayOfMonth;
-    }
+    // GOOD COMMENTS
+
+    // 1. LEGAL COMMENTS
+    // Copyright (c) 2011 by Osoco. All rights reserved.
+    // Released under the terms of the GNU General Public License
+    // Version 2 or later.
+
+    // 2. INFORMATIVE COMMENTS
+    // Returns an instance of the Responder being tested.
+    protected abstract Responder responderInstance();
+    // renaming teh function : responderBeingTested
+    // format matched kk:mm:ss EEE, MMM dd, yyyy
+    Pattern timeMatcher = Pattern.compile("\\d*:\\d*\\:d*\\:w*\\, \\w* \\d*, \\d*");
+
+    // 3. CLARIFICATION
+    assertTrue(a.compareTo(b) == -1); // a < b
+    assertTrue(b.compareTo(a) == 1); // b > a
 }
